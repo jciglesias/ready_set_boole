d723d295725dc0133ab80ba0eeb22229ce7d2b84
Juan Carlos Iglesias <jiglesia@student.42.fr>
2023-12-18 16:07:08 +0100

    modify if negative only

--- ex05/nnf.py
+++ ex05/nnf.py
@@ -1,12 +1,19 @@
 from string import ascii_uppercase
 
+n_dir = {
+    "&": lambda a, b: f"{a}{b}!|",
+    "|": lambda a, b: f"{a}{b}!&",
+    "=": lambda a, b: f"{a}{b}&{a}!{b}!&|",
+    "^": lambda a, b: f"{a}{b}^",
+    ">": lambda a, b: f"{a}{b}|",
+}
+
 eval_dir = {
-    "&": lambda a, b: f"{a}{b}|",
-    "|": lambda a, b: f"{a}{b}&",
+    "&": lambda a, b: f"{a}{b}&",
+    "|": lambda a, b: f"{a}{b}|",
     "=": lambda a, b: f"{a}{b}&{a}!{b}!&|",
     "^": lambda a, b: f"{a}{b}^",
     ">": lambda a, b: f"{a}!{b}|",
-    "!": lambda a: negation_form(a),
 }
 
 def negation_normal_form(formula: str) -> bool:
@@ -18,20 +25,29 @@ def negation_normal_form(formula: str) -> bool:
             elif elem in "&|^>=":
                 right = stack.pop()
                 left = stack.pop()
-                stack.append(eval_dir[elem](left, right))
+                stack.append(eval_dir[elem](negation_normal_form(left), right))
             elif elem == "!":
-                stack.append(eval_dir[elem](stack.pop()))
+                stack.append(negative_form(stack.pop(), 1))
             else:
                 print("bad character in formula: ", elem)
                 return
+            # print(stack)
         return stack.pop()
-    print("Formula should be a string bigger than 2 char")
+    return formula
 
-def negation_form(formula: str) -> str:
-    ret = ""
-    for a in formula:
-        if a in ascii_uppercase:
-            ret += a + "!"
-        else:
-            ret += a
-    return ret
+def negative_form(formula: str, negate: bool) -> str:
+    if isinstance(formula, str) and len(formula) > 2:
+        stack = []
+        for elem in formula:
+            if elem in ascii_uppercase:
+                stack.append(elem)
+            elif elem in "&|^>=":
+                right = stack.pop()
+                left = stack.pop()
+                if (negate):
+                    stack.append(n_dir[elem](negative_form(left, 0), right))
+                else:
+                    stack.append(left + "!" + right + "!" + elem)
+            # print(stack)
+        return stack.pop()
+    return formula + "!"
