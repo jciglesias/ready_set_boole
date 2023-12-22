8ec8366d1ddfcbf05f152d13b3f8609442b3ebad
Juan Carlos Iglesias <jiglesia@student.42.fr>
2023-12-22 16:33:15 +0100

    tree

--- ex05/nnf.py
+++ ex05/nnf.py
@@ -1,11 +1,13 @@
 from string import ascii_uppercase
+from copy import deepcopy
+import binarytree as bt
 
 n_dir = {
-    "&": lambda a, b: f"{a}{b}!|",
-    "|": lambda a, b: f"{a}{b}!&",
+    "&": lambda a, b: f"{a}!{b}!|",
+    "|": lambda a, b: f"{a}!{b}!&",
     "=": lambda a, b: f"{a}{b}&{a}!{b}!&|",
-    "^": lambda a, b: f"{a}{b}^",
-    ">": lambda a, b: f"{a}{b}|",
+    "^": lambda a, b: f"{a}!{b}^",
+    ">": lambda a, b: f"{a}!{b}|",
 }
 
 eval_dir = {
@@ -16,38 +18,84 @@ eval_dir = {
     ">": lambda a, b: f"{a}!{b}|",
 }
 
-def negation_normal_form(formula: str) -> bool:
+def negation_normal_form(f: str) -> str:
+    root = create_tree(f)
+    return tree_eval(root)
+
+def tree_eval(root: bt.Node) -> bt.Node:
+    if root:
+        root.left = tree_eval(root.left)
+        if root.value in "&|^=>":
+            if root.left.value in ascii_uppercase and root.right.value in ascii_uppercase:
+                return create_tree(eval_dir[root.value](root.left.value, root.right.value))
+        elif root.value == "!":
+            return negation_eval(root.left)
+    return root
+    
+
+def negation_eval(root: bt.Node) -> bt.Node:
+    if root:
+        root.left = negation_eval(root.left)
+        if root.value in "&|^=>":
+            if root.left.value in ascii_uppercase and root.right.value in ascii_uppercase:
+                return create_tree(n_dir[root.value](root.left.value, root.right.value))
+        elif root.value == "!":
+            return negation_eval(root.left)
+    return root
+
+def create_tree(formula: str):
     if isinstance(formula, str):
         stack = []
         for elem in formula:
-            if elem in ascii_uppercase:
-                stack.append(elem)
-            elif elem in "&|^>=":
+            node = bt.Node(elem)
+            if elem in "&|^>=":
                 try:
                     right = stack.pop()
                     left = stack.pop()
                 except:
                     exit("Bad formula")
-                stack.append(eval_dir[elem](negation_normal_form(left), right))
+                node.left = left
+                node.right = right
             elif elem == "!":
-                stack.append(negative_form(stack.pop(), 1))
-            else:
+                node.left = stack.pop()
+            elif elem not in ascii_uppercase:
                 exit(f"bad character in formula: {elem}")
+            stack.append(node)
         return stack.pop()
-    return formula
+    return
 
-def negative_form(formula: str, negate: bool) -> str:
-    if isinstance(formula, str) and len(formula) > 2:
-        stack = []
-        for elem in formula:
-            if elem in ascii_uppercase:
-                stack.append(elem)
-            elif elem in "&|^>=":
-                right = stack.pop()
-                left = stack.pop()
-                if (negate):
-                    stack.append(n_dir[elem](negative_form(left, 0), right))
-                else:
-                    stack.append(left + "!" + right + "!" + elem)
-        return stack.pop()
-    return formula + "!"
+# def negation_normal_form(formula: str) -> bool:
+#     if isinstance(formula, str):
+#         stack = []
+#         for elem in formula:
+#             if elem in ascii_uppercase:
+#                 stack.append(elem)
+#             elif elem in "&|^>=":
+#                 try:
+#                     right = stack.pop()
+#                     left = stack.pop()
+#                 except:
+#                     exit("Bad formula")
+#                 stack.append(eval_dir[elem](negation_normal_form(left), right))
+#             elif elem == "!":
+#                 stack.append(negative_form(stack.pop(), 1))
+#             else:
+#                 exit(f"bad character in formula: {elem}")
+#         return stack.pop()
+#     return formula
+
+# def negative_form(formula: str, negate: bool) -> str:
+#     if isinstance(formula, str) and len(formula) > 2:
+#         stack = []
+#         for elem in formula:
+#             if elem in ascii_uppercase:
+#                 stack.append(elem)
+#             elif elem in "&|^>=":
+#                 right = stack.pop()
+#                 left = stack.pop()
+#                 if (negate):
+#                     stack.append(n_dir[elem](negative_form(left, 0), right))
+#                 else:
+#                     stack.append(left + "!" + right + "!" + elem)
+#         return stack.pop()
+#     return formula + "!"
--- ex05/test.py
+++ ex05/test.py
@@ -1,4 +1,5 @@
 from nnf import negation_normal_form
+import binarytree as bt
 
 print(negation_normal_form("AB&!"))
 # A!B!|
@@ -10,3 +11,4 @@ print(negation_normal_form("AB="))
 # AB&A!B!&|
 print(negation_normal_form("AB|C&!"))
 # A!B!&C!|
+print(negation_normal_form("AB=!"))
