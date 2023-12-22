0af9b9a85572f658fc5200ce5c20c6ae5a483b96
Juan Carlos Iglesias <jiglesia@student.42.fr>
2023-12-22 11:38:59 +0100

    test compare

--- ex00/test.py
+++ ex00/test.py
@@ -3,4 +3,4 @@ from random import randrange
 
 if __name__=="__main__":
     for x, y in [(randrange(100), randrange(100)) for _ in range(10)]:
-        print(f"{x:2} + {y:2} = {adder(x,y)}")
+        print(f"{x:2} + {y:2} = {adder(x,y)}\t| {x + y}")
