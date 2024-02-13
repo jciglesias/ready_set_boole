d74b06a58f3f64f8db923b46319a1e3563064bbe
Juan Carlos Iglesias <jiglesia@student.42.fr>
2024-02-13 13:55:11 +0100

    test

--- ex08/test.py
+++ ex08/test.py
@@ -2,7 +2,12 @@ from powerset import powerset
 from random import sample
 
 if __name__=="__main__":
-    lst = [sample(range(1000), 5) for _ in range(5)]
-    pwset = powerset(lst)
-    for x in pwset:
-        print(x)
+    # lst = [sample(range(1000), 5) for _ in range(5)]
+    # pwset = powerset(lst)
+    # for x in pwset:
+    #     print(x)
+
+    print(powerset([]))
+    print(powerset([0]))
+    print(powerset([0,1]))
+    print(powerset([0,1,2]))
