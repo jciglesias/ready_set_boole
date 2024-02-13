32c1c3a1a85d48c38636ea22b05d364a377ddad9
Juan Carlos Iglesias <jiglesia@student.42.fr>
2024-02-13 13:09:22 +0100

    Basic and Composition test

--- ex07/test.py
+++ ex07/test.py
@@ -1,13 +1,21 @@
 from sat import sat
 
 if __name__=="__main__":
-    print(sat("AB|"))
-    # true
-    print(sat("AB&"))
-    # true
-    print(sat("AA!&"))
-    # false
-    print(sat("AA^"))
-    # false
-    print(sat("A!"))
-    # true
+    # Basic tests
+    print("OK" if sat("A") == True else "KO") # true
+    print("OK" if sat("A!") == True else "KO") # true
+    print("OK" if sat("AA|") == True else "KO") # true
+    print("OK" if sat("AA&") == True else "KO") # true
+    print("OK" if sat("AA!&") == False else "KO") # false
+    print("OK" if sat("AA^") == False else "KO") # false
+    print("OK" if sat("AB^") == True else "KO") # true
+    print("OK" if sat("AB=") == True else "KO") # true
+    print("OK" if sat("AA>") == True else "KO") # true
+    print("OK" if sat("AA!") == True else "KO") # true
+    
+    # Composition
+    print("OK" if sat("ABC||") == True else "KO") # true
+    print("OK" if sat("AB&A!B!&&") == False else "KO") # false
+    print("OK" if sat("ABCDE&&&&") == True else "KO") # true
+    print("OK" if sat("AAA^^") == True else "KO") # true
+    print("OK" if sat("ABCDE^^^^") == True else "KO") # true
